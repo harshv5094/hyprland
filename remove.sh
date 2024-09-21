8e48c8147b31e3cefccfae87b2bd77c43b25d778
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-09-21 13:05:42 +0530

    feat: Upgrading setup and remove script
    Since setup is small i'm removing stow and modifying script to be more
    rebust

---
+++ remove.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+CYAN=$(tput setaf 6)
+RESET=$(tput sgr0)
+
+cmdCheck() {
+  if [ $? -eq 0 ]; then
+    printf "%b\n" "${GREEN}**SUCCESS**${RESET}"
+  else
+    printf "%b\n" "${RED}**FAIL**${RESET}"
+  fi
+}
+
+checkFolderStatus() {
+  dir_paths=("hypr" "dunst" "waybar" "waypaper" "wlogout" "wofi")
+  printf "%b\n" "${CYAN}Checking folder status${RESET}"
+  for folder in "${dir_paths[@]}"; do
+    if [ -e "$HOME/.config/$folder" ]; then
+      printf "%b\n" "${YELLOW} ***$HOME/.config/$folder exist*** ${RESET}"
+      printf "%b\n" "${RED} **Removing the existing $folder directory** ${RESET}"
+      rm -rf "$HOME/.config/$folder"
+      cmdCheck
+    else
+      printf "%b\n" "${RED} ***$HOME/.config/$folder does not exist*** ${RESET}"
+    fi
+  done
+}
+
+packageRemove() {
+  packages=$(grep -vE "^\s#" "$HOME/hyprland/packages.txt" | tr "\n" " ")
+  printf "\n%b\n" "${CYAN} **Installing ${RED}$packages${RESET}${CYAN}** ${RESET}"
+  if command -v dnf &>/dev/null; then
+    sudo dnf remove $packages
+  fi
+}
+
+checkFolderStatus
+packageRemove
