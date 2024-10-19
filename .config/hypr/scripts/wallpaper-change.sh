297577cbac8eff75fe96434907166dd3a8acf627
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-19 10:28:55 +0530

    Revert "refactor(wallp*.sh): changing options position"
    This reverts commit 31ed6aac6dc5fa66a000a0d10bd64983ef5aa08a.

--- .config/hypr/scripts/wallpaper-change.sh
+++ .config/hypr/scripts/wallpaper-change.sh
@@ -3,6 +3,8 @@
 hyprctl hyprpaper unload all
 killall hyprpaper
 
+echo "splash = false" >~/.config/hypr/hyprpaper.conf
+echo "ipc = on" >>~/.config/hypr/hyprpaper.conf
 monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
 for monitor in $monitors; do
@@ -10,8 +12,5 @@ for monitor in $monitors; do
   echo "preload = $wallpaper" >>~/.config/hypr/hyprpaper.conf
   echo "wallpaper = $monitor,$wallpaper" >>~/.config/hypr/hyprpaper.conf
 done
-
-echo "splash = false" >~/.config/hypr/hyprpaper.conf
-echo "ipc = true" >>~/.config/hypr/hyprpaper.conf
 
 hyprpaper &
