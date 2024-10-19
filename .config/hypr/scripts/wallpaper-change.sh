31ed6aac6dc5fa66a000a0d10bd64983ef5aa08a
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-19 10:27:46 +0530

    refactor(wallp*.sh): changing options position

--- .config/hypr/scripts/wallpaper-change.sh
+++ .config/hypr/scripts/wallpaper-change.sh
@@ -3,8 +3,6 @@
 hyprctl hyprpaper unload all
 killall hyprpaper
 
-echo "splash = false" >~/.config/hypr/hyprpaper.conf
-echo "ipc = on" >>~/.config/hypr/hyprpaper.conf
 monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
 for monitor in $monitors; do
@@ -12,5 +10,8 @@ for monitor in $monitors; do
   echo "preload = $wallpaper" >>~/.config/hypr/hyprpaper.conf
   echo "wallpaper = $monitor,$wallpaper" >>~/.config/hypr/hyprpaper.conf
 done
+
+echo "splash = false" >~/.config/hypr/hyprpaper.conf
+echo "ipc = true" >>~/.config/hypr/hyprpaper.conf
 
 hyprpaper &
