b20169f8247dfedaef9f25be29ed2e738b0bafbe
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-20 00:06:30 +0530

    chore(wall*.sh): adding random wallpaper for diff display code

--- .config/hypr/scripts/wallpaper-change.sh
+++ .config/hypr/scripts/wallpaper-change.sh
@@ -5,6 +5,13 @@ killall hyprpaper
 
 echo "splash = false" >~/.config/hypr/hyprpaper.conf
 echo "ipc = true" >>~/.config/hypr/hyprpaper.conf
+# monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
+
+# for monitor in $monitors; do
+#   wallpaper=$(fd ".png|.jpg|.jpeg|.webp" ~/hyprland/wall/ | shuf -n1)
+#   echo "preload = $wallpaper" >>~/.config/hypr/hyprpaper.conf
+#   echo "wallpaper = $monitor,$wallpaper" >>~/.config/hypr/hyprpaper.conf
+# done
 
 wallpaper=$(fd ".png|.jpg|.jpeg|.webp" ~/hyprland/wall/ | shuf -n1)
 echo "preload = $wallpaper" >>~/.config/hypr/hyprpaper.conf
